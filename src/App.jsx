3081d80341cdfd57ee0ec49921cb3c1574da289c
Deividas <kliucevicd@gmail.com>
2024-08-22 11:44:45 +0300

    simple-to-do-list

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react';
+import ToDoList from './assets/ToDoList';
+import './App.css';
+
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+     
+        <ToDoList />
+   
+
+
+    </>
+  );
+}
+
+export default App;
